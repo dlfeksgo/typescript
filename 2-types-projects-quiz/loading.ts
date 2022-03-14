1262254ea5722607fbe20329ef9473429c04c565
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-03-14 21:40:49 +0900

    로딩 상태 표시 연습

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -1,26 +1,36 @@
 {
-  /**
-   * Print Loading State
-   */
-  type LoadingState = {
-    state: 'loading';
-  };
+	/**
+	 * Print Loading State
+	 */
+	type LoadingState = {
+		state: 'loading';
+	};
 
-  type SuccessState = {
-    state: 'success';
-    response: {
-      body: string;
-    };
-  };
+	type SuccessState = {
+		state: 'success';
+		response: {
+			body: string;
+		};
+	};
 
-  type FailState = {
-    state: 'fail';
-    reason: string;
-  };
+	type FailState = {
+		state: 'fail';
+		reason: string;
+	};
 
-  type ResourceLoadState = LoadingState | SuccessState | FailState;
+	type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+	function printLoginState(stateTest: ResourceLoadState) {
+		if (stateTest.state === 'loading') {
+			console.log('👀 loading...');
+		} else if (stateTest.state === 'success') {
+			console.log(`😃 ${stateTest.response.body}`);
+		} else if (stateTest.state === 'fail') {
+			console.log(`😱 ${stateTest.reason}`);
+		}
+	}
+
+	printLoginState({ state: 'loading' }); // 👀 loading...
+	printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
+	printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
 }
