765d98e6ab52a72114748979d3d43a04f5681558
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2022-03-16 15:59:16 +0900

    getter와 setter

--- 3-3-encapsulation.ts
+++ 3-3-encapsulation.ts
@@ -36,7 +36,28 @@
 		}
 	}
 
-	const maker = new CoffeeMaker(0);
-	maker.fillCoffeeBeans(40);
-	console.log(maker);
+	// const maker = new CoffeeMaker(0);
+	// maker.fillCoffeeBeans(40);
+	// console.log(maker);
+
+	class User {
+		get fullName(): string {
+			return `${this.firstName} ${this.lastName}`;
+		}
+
+		private internalAge = 4;
+		get age(): number {
+			return this.internalAge;
+		}
+		set age(num: number) {
+			if (num < 0) {
+				console.log('다시 입력해');
+			}
+			this.internalAge = num;
+		}
+		constructor(private firstName: string, public lastName: string) {}
+	}
+
+	const user = new User('Steve', 'Jobs');
+	user.age = 7;
 }
